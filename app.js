07b03bee0f9f1d7b7e19bc36cf5a9f09c7b84890
rachit <rachitgarg50@gmail.com>
2018-07-09 15:55:51 +0530

    CRUD fun. Added

--- app.js
+++ app.js
@@ -1,13 +1,18 @@
+var bodyParser = require('body-parser');
 var express = require('express');
 var path = require('path');
-var rooms = require('./data/rooms.json')
+var rooms = require('./data/rooms.json');
+var uuid = require('node-uuid');
 var app = express();
+var _ = require('lodash');
 
 app.set("views", "./views");
 app.set("view engine","jade");
+
+
 app.use(express.static(path.join(__dirname, 'public')));
 //app.use(express.static(path.join(__dirname, 'admin')));
-
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/',(req, res)=>{
   res.render('index',{title : "Home"});
@@ -20,6 +25,46 @@ app.get('/admin/rooms',(req, res)=>{
 });
 
 
+app.get('/admin/rooms/add',(req,res)=>{
+  res.render("add");
+});
+
+app.post('/admin/rooms/add',(req,res)=>{
+  var room = {
+    name: req.body.name,
+    id: uuid.v4()
+  };
+
+  rooms.push(room);
+  res.redirect("/admin/rooms");
+});
+
+app.get('/admin/rooms/edit/:id',(req,res)=>{
+  var roomId = req.params.id;
+  var room  = _.find(rooms, r=> r.id ==roomId);
+  if(!room){
+    res.sendStatus(404);
+    return;
+  }
+  res.render('edit',{room});
+});
+
+app.post('/admin/rooms/edit/:id',(req,res)=>{
+  var roomId = req.params.id;
+  var room  = _.find(rooms, r=> r.id ==roomId);
+  if(!room){
+    res.sendStatus(404);
+    return;
+  }
+  room.name = req.body.name;
+  res.redirect("/admin/rooms");
+});
+
+app.get('/admin/rooms/delete/:id',(req,res)=>{
+  var roomId = req.params.id;
+  rooms  = rooms.filter(r=> r.id !=roomId);
+  res.redirect("/admin/rooms");
+});
 
 app.listen(3000,()=>{
   console.log("Chat app is listening on port number 3000!")
